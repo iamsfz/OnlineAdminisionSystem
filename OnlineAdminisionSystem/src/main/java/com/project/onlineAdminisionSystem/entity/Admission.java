8537edd41f69198b2d036fed187ad462c847f26c
Ajithreddy26 <69136890+Ajithreddy26@users.noreply.github.com>
2021-06-09 10:23:12 +0530

    Update Admission.java

--- OnlineAdminisionSystem/src/main/java/com/project/onlineAdminisionSystem/entity/Admission.java
+++ OnlineAdminisionSystem/src/main/java/com/project/onlineAdminisionSystem/entity/Admission.java
@@ -14,8 +14,7 @@ public class Admission
 	private String applicationId;
 	private String admissionStatus;//Confirm after payement done;
 	private College college;
-	/*private Program program;
-	private String year;*/
+	
 	
 	public int getAdmissionId() {
 		return admissionId;
