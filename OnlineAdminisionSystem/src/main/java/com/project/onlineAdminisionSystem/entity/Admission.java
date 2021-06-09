87f3b0de966b88faf5c03afc50f2d2b22c4d2b0f
Ajithreddy26 <69136890+Ajithreddy26@users.noreply.github.com>
2021-06-09 10:22:12 +0530

    Add files via upload

---
+++ OnlineAdminisionSystem/src/main/java/com/project/onlineAdminisionSystem/entity/Admission.java
@@ -0,0 +1,72 @@
+package com.project.onlineAdminisionSystem.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Admission
+{
+	@Id
+	@GeneratedValue
+	private int admissionId;
+	private String emailId;
+	private String applicationId;
+	private String admissionStatus;//Confirm after payement done;
+	private College college;
+	/*private Program program;
+	private String year;*/
+	
+	public int getAdmissionId() {
+		return admissionId;
+	}
+	public void setAdmissionId(int admissionId) {
+		this.admissionId = admissionId;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getApplicationId() {
+		return applicationId;
+	}
+	public void setApplicationId(String applicationId) {
+		this.applicationId = applicationId;
+	}
+	public String getAdmissionStatus() {
+		return admissionStatus;
+	}
+	public void setAdmissionStatus(String admissionStatus) {
+		this.admissionStatus = admissionStatus;
+	}
+	public College getCollege() {
+		return college;
+	}
+	public void setCollege(College college) {
+		this.college = college;
+	}
+	public Program getProgram() {
+		return program;
+	}
+	public void setProgram(Program program) {
+		this.program = program;
+	}
+	public Course getCourse() {
+		return course;
+	}
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	private Program program;
+	private Course course;
+	private String year;
+
+}
